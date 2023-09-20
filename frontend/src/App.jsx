d323e4b4f343c173ffd4bcb8d4999ecf23374611
Sayali <saykaw16@gmail.com>
2023-09-20 23:14:55 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Routes,Route} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className = 'bg-red-400 text-white'>App</div>
+  )
+}
+
+export default App
